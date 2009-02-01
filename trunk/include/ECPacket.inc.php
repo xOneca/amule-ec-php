9c8bc70cbd83c08e71edf9d73f96acaf05824f45
xoneca <xoneca@ea2ac73e-eed5-11dd-af78-c1de93f583f4>
2009-02-01 00:49:24 +0000

    ECPacket.inc.php: parse_packet() function started

--- trunk/include/ECPacket.inc.php
+++ trunk/include/ECPacket.inc.php
@@ -14,10 +14,34 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 
 // Purpose:
-// Class for implementing a packet
+// Class implementing a packet
 
+require_once('ECCodes.inc.php');
+
+// Class for managing packets
 class ECPacket
 {
     var $flags = 0x20;
-    var $opcode = 0x0;
+    var $opcode = 0;
+    var $datasize = 0;
+    var $accepts = 0;
+
+    function __construct(){}
+
+    // Parse raw packet to get the information
+    function parse_packet($str)
+    {
+        list(,$this->flags) = unpack('N', $str);
+        $str = substr($str, 4);
+        if(($this->flags & EC_FLAG_ACCEPTS) == EC_FLAG_ACCEPTS)
+        {
+            list(,$this->accepts) = unpack('N', $str);
+        }
+    }
 }
+
+// $p = new ECPacket();
+// $p->parse_packet(pack('N2', 0x30, 0x23));
+// var_dump(bin2hex(pack('N2', 0x30, 0x23)));
+// var_dump($p->flags);
+// var_dump($p->accepts);
